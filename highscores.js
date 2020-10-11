6f35353ef0df75040f4519f521afad51d3709e4b
Felix Atsu Kofi Ackuayi <felixackuayi@aol.com>
2020-10-11 09:36:28 +0000

    adding feature to save high scores in Local storage

---
+++ highscores.js
@@ -0,0 +1,14 @@
+const highScoresList = document.getElementById("highScoresList");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+/*highScores.map(score => {
+        // console.log(score);
+    })*/
+
+highScoresList.innerHTML = highScores
+  .map(score => {
+
+     // console.log(score);
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");
